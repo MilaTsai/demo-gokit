bd8474eb764ff840d55030cd3831a021a1afa983
mila <emily0912771607@gmail.com>
2020-11-09 16:34:28 +0800

    Endpoint V01

--- Services/UserEnpoint.go
+++ Services/UserEnpoint.go
@@ -1,18 +1,25 @@
 package Services
 
-import "github.com/go-kit/kit/endpoint"
+import (
+	"context"
+	"github.com/go-kit/kit/endpoint"
+)
 
 type UserRequest struct {
-
 	Uid int `json:"uid"`
 }
 
 type UserResponse struct {
-
 	Result string `json:"result"`
-
 }
 
-func GenUserEnpoint() endpoint.Endpoint {
+func GenUserEnpoint(userService IUserService) endpoint.Endpoint {
 
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+
+		r := request.(UserRequest)
+		result := userService.GetName(r.Uid)
+
+		return UserResponse{Result: result}, nil
+	}
 }
